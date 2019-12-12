03c534e541a72004440936043fdce7b9c6005b41
chenxianzhong <chenxianzhong@1230>
2019-12-12 15:03:17 +0800

    快速排序

--- algorithm/src/com/data/sort/QuickSort.java
+++ algorithm/src/com/data/sort/QuickSort.java
@@ -26,32 +26,52 @@ public class QuickSort {
      */
     public static void quickSort(int[] arr, int start, int end) {
         if (start < end) {
-            //把数组中的第0个作为标准数
+            //获取基准数
             int stard = arr[start];
-            //记录需要排序的下标
-            int low = start; //低
-            int high = end; //高
-            //循环找比标准数大的数和比标准数小的数
+            //开始值
+            int low = start;
+            int high = end;
             while (low < high) {
-                //右边的数字比标准数大
                 while (low < high && stard <= arr[high]) {
                     high--;
                 }
-                //使用右边的数字替换左边的数
                 arr[low] = arr[high];
-                //如果左边的数字比标准数小
-                while (low < high && arr[low] < stard) {
+                while (low < high && stard > arr[low]) {
                     low++;
                 }
-                //使用左边的数字替换右边的数字
                 arr[high] = arr[low];
-                //把标准数赋给低所在的位置
                 arr[low] = stard;
             }
-            //处理所有小的数字
             quickSort(arr, start, low);
-            //处理所有大的数字
             quickSort(arr, low + 1, end);
         }
+//        if (start < end) {
+//            //把数组中的第0个作为标准数
+//            int stard = arr[start];
+//            //记录需要排序的下标
+//            int low = start; //低
+//            int high = end; //高
+//            //循环找比标准数大的数和比标准数小的数
+//            while (low < high) {
+//                //右边的数字比标准数大
+//                while (low < high && stard <= arr[high]) {
+//                    high--;
+//                }
+//                //使用右边的数字替换左边的数
+//                arr[low] = arr[high];
+//                //如果左边的数字比标准数小
+//                while (low < high && arr[low] < stard) {
+//                    low++;
+//                }
+//                //使用左边的数字替换右边的数字
+//                arr[high] = arr[low];
+//                //把标准数赋给低所在的位置
+//                arr[low] = stard;
+//            }
+//            //处理所有小的数字
+//            quickSort(arr, start, low);
+//            //处理所有大的数字
+//            quickSort(arr, low + 1, end);
+//        }
     }
 }
